bbe28e6a8442db7b5d8b8396fce7872e0c749f69
Trond Norbye <trond.norbye@gmail.com>
2016-01-26 16:19:27 +0100

    Add support to test over the wire to Couchbase

--- src/com/couchbase/security/sasl/test/Main.java
+++ src/com/couchbase/security/sasl/test/Main.java
@@ -25,19 +25,160 @@ import javax.security.sasl.SaslClient;
 import javax.security.sasl.SaslException;
 import javax.security.sasl.SaslServer;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
 
 /**
  * Test program used to test the SCRAM-SHAx implementations ;-)
  */
 public class Main implements CallbackHandler {
+    private void sendPacket(Socket socket, int opcode, String key, byte[] payload) throws IOException {
+        ByteBuffer header = ByteBuffer.allocate(24);
+        header.rewind();
+        header.put((byte)(0x80 & 0xff));
+        header.put((byte)(opcode & 0xff));
+        header.putShort((short)(key.length() & 0xffff));
+        header.put((byte)0); // extlen
+        header.put((byte)0); // datatype
+        header.putShort((short)0); // vbucket
+        header.putInt(key.length() + payload.length);
+        OutputStream out = socket.getOutputStream();
+        out.write(header.array());
+        out.write(key.getBytes());
+        out.write(payload);
+    }
+
+    private final static int SASL_LIST_MECH = 0x20;
+    private final static int SASL_AUTH = 0x21;
+    private final static int SASL_STEP = 0x22;
+
+
+    private void sendSaslAuth(Socket socket,
+                             String mechanism,
+                             byte[] payload) throws IOException {
+        sendPacket(socket, SASL_AUTH, mechanism, payload);
+    }
+
+    private void sendSaslStep(Socket socket,
+                              String mechanism,
+                              byte[] payload) throws IOException {
+        sendPacket(socket, SASL_STEP, mechanism, payload);
+    }
+
+    private String saslListMech(InetSocketAddress address) {
+        try {
+            Socket socket = new Socket();
+            socket.connect(address);
+            sendPacket(socket, SASL_LIST_MECH, "", new byte[0]);
+            ByteBuffer response = readResponse(socket);
+
+            if (response.getInt(6) != 0) {
+                throw new RuntimeException("LIST MECH failed: " + response.getInt(6));
+            }
+
+            response.position(24);
+            byte[] mech = new byte[response.getInt(8)];
+            response.get(mech);
+
+            return new String(mech);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return "PLAIN";
+    }
+
+    private ByteBuffer readResponse(Socket socket) throws IOException {
+        InputStream in = socket.getInputStream();
+        byte[] header = new byte[24];
+        if (in.read(header) != 24) {
+            throw new RuntimeException("Incomplete read");
+        }
+
+        ByteBuffer buffer = ByteBuffer.wrap(header);
+        int bodylength = buffer.getInt(8);
+        byte[] payload = new byte[bodylength];
+        if (in.read(payload) != bodylength) {
+            throw new RuntimeException("Incomplete read");
+        }
+
+        ByteBuffer ret = ByteBuffer.allocate(24 + bodylength);
+        ret.rewind();
+        ret.put(header);
+        ret.put(payload);
+        return ret;
+    }
+
+    private void testServer(String mechanism, SocketAddress address){
+        System.out.println("Testing mechanism: " + mechanism);
+
+        try {
+            Socket socket = new Socket();
+            socket.connect(address);
+
+            String[] mechs = {mechanism};
+
+            SaslClient client = Sasl.createSaslClient(mechs, null, "couchbase",
+                        "127.0.0.1", null, this);
+
+            if (client == null) {
+                System.err.println("Failed to create client");
+                return;
+            }
+
+            byte[] array = new byte[0];
+            if (client.hasInitialResponse()) {
+                array = client.evaluateChallenge(array);
+            }
+
+            sendSaslAuth(socket, mechanism, array);
+            do {
+                ByteBuffer response = readResponse(socket);
+                short status = response.getShort(6);
+                if (status != 0 && status != 0x21) {
+                    throw new RuntimeException("SASL AUTH failed with" +
+                            Integer.toHexString(status) + " (" + mechanism + ")");
+                }
+
+                int bodyLength = response.getInt(8);
+                if (bodyLength > 0) {
+                    array = new byte[bodyLength];
+                    response.position(24);
+                    response.get(array, 0, bodyLength);
+
+                    array = client.evaluateChallenge(array);
+                }
+
+                if (status == 0x21) {
+                    // the server is expecting something
+                    sendSaslStep(socket, mechanism, array);
+                } else {
+                    if (!client.isComplete()) {
+                     throw new RuntimeException("I expected the client to be done.. " +
+                             "the server is not waiting");
+                    }
+                }
+            } while (!client.isComplete());
+
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void test(String scram) {
         String[] mechs = {scram, "CRAM-MD5", "PLAIN"};
 
         try {
             SaslClient client = Sasl.createSaslClient(mechs, null, "couchbase",
-                                                      "127.0.0.1", null, this);
+                    "127.0.0.1", null, this);
             SaslServer server = Sasl.createSaslServer(mechs[0], "couchbase",
-                                                      "127.0.0.1", null, this);
+                    "127.0.0.1", null, this);
 
             if (client == null) {
                 System.err.println("Failed to create client");
@@ -59,7 +200,7 @@ public class Main implements CallbackHandler {
 
             if (!client.isComplete() || !server.isComplete()) {
                 System.err.println("client or server doesn't think " +
-                                           "they're done");
+                        "they're done");
             }
         } catch (SaslException e) {
             e.printStackTrace();
@@ -72,6 +213,16 @@ public class Main implements CallbackHandler {
         o.test("SCRAM-SHA1");
         o.test("SCRAM-SHA256");
         o.test("SCRAM-SHA512");
+
+
+        // If you've started a couchbase server with cluster-run and have a bucket named foo with password
+        // pencil the following things should also work...
+        String serverMechs = o.saslListMech( new InetSocketAddress("localhost", 12000));
+        String[] mechs = serverMechs.split(" ");
+        for (String m : mechs) {
+            o.testServer(m, new InetSocketAddress("localhost", 12000));
+        }
+
         System.exit(0);
     }
 
@@ -91,5 +242,3 @@ public class Main implements CallbackHandler {
         }
     }
 }
-
-
